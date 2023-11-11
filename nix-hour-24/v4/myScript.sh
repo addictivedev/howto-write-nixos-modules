9071c9eaf496898b8f0d4afdaa581779a866b3bf
Nicola Brisotto <nicolabrisotto@gmail.com>
2023-11-11 14:59:22 +0100

    cp v3 to v4

---
+++ nix-hour-24/v4/myScript.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+passwordFile=$1
+while true; do
+    cat "$passwordFile"
+done
---
+++ nix-hour-24/v4/vm-test.nix
@@ -0,0 +1,26 @@
+{ pkgs ? import <nixpkgs> {} }:  
+pkgs.nixosTest {
+  name = "myService";
+  nodes = {
+    machine = {
+      imports = [
+        ./myService.nix
+      ];
+
+      services.myService.enable = true;
+      services.myService.passwordFile = pkgs.writeText "myPassword" "ThisIsMyPassword";
+    };
+  };
+
+  testScript = ''
+    #### PYTHON CODE for TESTING###
+    # we start all the machines
+    start_all()
+
+    # wait for the systemd unit on the node "machine"
+    machine.wait_for_unit("myService.service")
+
+    # we grep the log message "foo" that we expect
+    machine.succeed("journalctl -u myService.service --grep='ThisIsMyPassword'")
+  '';
+}
